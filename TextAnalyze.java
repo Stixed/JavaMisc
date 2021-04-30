1272f977be45bed8f12f26f59a3200151177b80e
Stanislav Kolodny <31438573+Stixed@users.noreply.github.com>
2021-04-30 12:15:27 +0300

    Update TextAnalyze.java

--- TextAnalyze.java
+++ TextAnalyze.java
@@ -1,27 +1,55 @@
 public Label checkLabels(TextAnalyzer[] analyzers, String text) {
-    enum Label {
+    
+}  
+enum Label {
         SPAM, NEGATIVE_TEXT, TOO_LONG, OK
-    }
-    abstract class KeywordAnalyzer {
-        abstract String[] getKeywords();
-        abstract int getLabel();
-    }
-    class SpamAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
-        private String[] keywords;
-        public SpamAnalyzer(String[] keywords) {
-            this.keywords = keywords.clone();
-        }
-    }
+}
+abstract class KeywordAnalyzer {
+    protected abstract String[] getKeywords();
+    protected abstract Label getLabel();
     
-    class NegativeTextAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
-    
-    }
-    
-    public class TooLongTextAnalyzer implements TextAnalyzer {
+    public processText() {
     
     }
 }
+class SpamAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
+    private String[] keywords;
 
-
+    public SpamAnalyzer(String[] keywords) {
+        this.keywords = keywords.clone();
+    }
+    
+    @Override
+    public String[] getKeywords() {
+        return this.keywords;
+    }
+    @Override
+    public Label getLabel() {
+        return Label.SPAM;
+    }
+}
+    
+class NegativeTextAnalyzer extends KeywordAnalyzer implements TextAnalyzer {
+    private String[] keywords =  {":(" ,"=(" ,":|"};
+    
+    @Override
+    public String[] getKeywords() {
+        return this.keywords;
+    }
+    @Override
+    public Label getLabel() {
+        return Label.NEGATIVE_TEXT;
+    }
+}
+    
+public class TooLongTextAnalyzer implements TextAnalyzer {
+    private int maxLength;
+        
+    public TooLongTextAnalyzer(maxLength) {
+        this.maxLength = maxLength;
+    }
+    public Label processText() {
+    }
+}
 
 
